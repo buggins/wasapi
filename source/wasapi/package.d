994b04f1f6dfa842a73156a203a532ae0f316b47
Vadim Lopatin <coolreader.org@gmail.com>
2016-10-06 09:37:48 +0300

    publish Core Audio bindings as DUB project

---
+++ source/wasapi/package.d
@@ -0,0 +1,8 @@
+/**
+    Translation Windows of Core Audio interfaces: MMDevice, WASAPI, EndpointVolume API header files.
+*/
+module wasapi;
+
+public import wasapi.coreaudio;
+public import wasapi.comutils;
+
